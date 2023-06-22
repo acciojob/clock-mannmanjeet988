68da9ac37938cea76f8eda79da2c6b4ffb504a81
mannmanjeet988 <120735535+mannmanjeet988@users.noreply.github.com>
2023-06-22 19:08:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+    // Function to update the time and date dynamically
+    function updateTimeAndDate() {
+      const timerElement = document.getElementById("timer");
+      const currentDate = new Date();
+
+      // Format the time and date
+      const timeString = currentDate.toLocaleTimeString();
+      const dateString = currentDate.toDateString();
+
+      // Update the content of the <p> element
+      timerElement.textContent = `${timeString} - ${dateString}`;
+    }
+
+    // Call the function initially to display the current time and date
+    updateTimeAndDate();
+
+    // Update the time and date every second (1000 milliseconds)
+    setInterval(updateTimeAndDate, 1000);
+  
